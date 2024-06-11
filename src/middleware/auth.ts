dcef332fb36b0b4baa882fb0541c70ea72c24a42
Spandan Barve <spandan567@gmail.com>
2024-06-11 18:40:28 +0530

    Add auth midlware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,39 @@
+import { Express, Request, Response, NextFunction } from "express";
+import { getAuthTokenFromHeader } from "../utils";
+import jwt from "jsonwebtoken";
+import { User } from "../types/custom";
+
+export function authorisedOnly(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = getAuthTokenFromHeader(req);
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(200).send({ invalidToken: true });
+
+    req.user = user as User;
+
+    if (!req.user) return res.status(200).send({ invalidToken: true });
+
+    next();
+  });
+}
+
+export function unauthorisedOnly(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = getAuthTokenFromHeader(req);
+
+  if (!token) return next();
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (!err) return res.sendStatus(403);
+    next();
+  });
+}
--- src/routes/token.ts
+++ src/routes/token.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/new", (req, res) => {
+router.post("/", (req, res) => {
   res.send({ message: "success" });
 });
 
