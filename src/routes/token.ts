8912261247a4795a7d6b8d0e632201b777dfb9aa
Spandan Barve <spandan567@gmail.com>
2024-06-12 21:28:28 +0530

    remove auth middleare

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -1,37 +1,7 @@
 import express from "express";
-import { authorisedOnly } from "../middleware/auth";
 import User from "../models/User";
 import evm from "../../evm";
 const router = express.Router();
-
-router.post("/enqueue", authorisedOnly, async (req, res) => {
-  const { name, symbol, image, website, description, telegram, twitter } =
-    req.body;
-
-  if (!req.user) return res.sendStatus(401);
-
-  const creator = req.user.address;
-
-  const user = await User.findOne({ address: creator });
-
-  user?.queue.push({
-    name,
-    symbol,
-    image,
-    website,
-    description,
-    telegram,
-    twitter,
-  });
-
-  if ((user?.queue?.length || 0) > 5) {
-    await User.findOneAndUpdate({ address: creator }, { $pop: { tokens: -1 } });
-  }
-
-  await user?.save();
-
-  return res.status(200).send({ success: true });
-});
 
 router.post("/new", authorisedOnly, async (req, res) => {
   if (!req.user?.address) return res.sendStatus(401);
