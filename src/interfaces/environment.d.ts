8acbec999fcddc41194b9bd3feddf52486223feb
Spandan Barve <spandan567@gmail.com>
2024-06-13 19:19:00 +0530

    changes

--- src/interfaces/environment.d.ts
+++ src/interfaces/environment.d.ts
@@ -4,12 +4,9 @@ declare global {
       NODE_ENV: "development" | "production";
       FRONTEND_URL: "http://127.0.01:5173";
       PORT?: number;
-<<<<<<< HEAD
       MONGODB_URI: string;
-=======
       ACCESS_TOKEN_SECRET: string;
-      HUDDLE_API_KEY: string
->>>>>>> 24c95cdf3e8c319e751fb05975f039972b920e0f
+      HUDDLE_API_KEY: string;
     }
   }
 }
