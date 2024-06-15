8ce2a5c42c98e3f8ddf4a9b5b75d83d377e8893a
Riya Jain <riya1jain567@gmail.com>
2024-06-15 16:40:19 +0530

    add huddle progression

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,3 +15,16 @@ export function getAuthTokenFromHeader(req: any): string | false {
   if (token == null) return false;
   return token;
 }
+
+export function generateRandomHex(length: number): string {
+  const characters = "0123456789abcdef";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+export function isFunction(obj: any) {
+  return typeof obj === "function";
+}
