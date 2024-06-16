ddd9f559a255fa07aec40c5b52d64eceaf5ced60
Spandan Barve <spandan567@gmail.com>
2024-06-16 15:02:07 +0530

    update ca

--- config.ts
+++ config.ts
@@ -2,3 +2,5 @@ export const frontendUrl =
   process.env.NODE_ENV === "development" || !process.env.FRONTEND_URL
     ? "localhost:5173"
     : process.env.FRONTEND_URL;
+
+export const ONE_FRAX = BigInt(Math.pow(10, 18));
--- src/models/PriceFeed.ts
+++ src/models/PriceFeed.ts
@@ -3,7 +3,7 @@ import { PriceFeed } from "../types/custom";
 
 const priceFeedSchema = new Schema<PriceFeed>({
   address: { type: String, unique: true },
-  data: { type: [{ marketCap: Number, price: Number, timestamp: Number }] },
+  data: { type: [{ marketCap: BigInt, price: BigInt, timestamp: BigInt }] },
   lastRefreshedBlock: { type: BigInt },
 });
 
