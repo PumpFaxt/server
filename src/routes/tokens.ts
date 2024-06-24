79f3467138b00c7d2670abc43a2d827a16515ffa
Spandan Barve <spandan567@gmail.com>
2024-06-25 01:10:42 +0530

    refactor tokens listing endpoint

--- src/routes/tokens.ts
+++ src/routes/tokens.ts
@@ -9,71 +9,6 @@ import PriceFeed from "../models/PriceFeed";
 import { ONE_FRAX } from "../../config";
 const router = express.Router();
 
-router.post("/refresh", async (req, res) => {
-  try {
-    const config = await Config.findOne({});
-    if (!config) throw "No config found";
-
-    const tokens = await evm.client.getContractEvents({
-      ...contracts.pumpItFaxtInterface,
-      eventName: "Launch",
-      fromBlock: config.tokensLastBlock,
-      toBlock: "latest",
-    });
-
-    tokens.forEach(async (item) => {
-      const tokenAddress = item.args.token;
-
-      if (!tokenAddress) return;
-
-      const token = getContract({
-        abi: contracts.token.abi,
-        address: tokenAddress,
-        client: evm.client,
-      });
-
-      const metadata = JSON.parse(await token.read.metadata());
-
-      await Token.deleteOne({ address: token.address });
-
-      const newToken = await Token.create({
-        address: token.address,
-        creator: await token.read.creator(),
-        name: await token.read.name(),
-        totalSupply: Number((await token.read.totalSupply()) / evm.ONE_TOKEN),
-        symbol: await token.read.symbol(),
-        image: await token.read.image(),
-        description: metadata.description,
-        telegram: metadata.telegram,
-        twitter: metadata.twitter,
-        website: metadata.website,
-      });
-
-      await PriceFeed.deleteOne({ address: token.address });
-
-      const priceFeed = await PriceFeed.create({
-        address: token.address,
-        lastRefreshedBlock: await evm.getBlockNumber(),
-        data: [{ time: Date.now() - 1000 * 60, mktCap: 0, value: 0 }],
-      });
-
-      await priceFeed.save();
-      await newToken.save();
-      const updatedConfig = await Config.findOneAndUpdate(
-        {},
-        { $set: { tokensLastBlock: item.blockNumber } }
-      );
-      updatedConfig?.save();
-    });
-
-    return res.status(200).send({ message: "Success" });
-  } catch (err) {
-    console.error(err);
-
-    return res.sendStatus(500);
-  }
-});
-
 router.get("/", async (req, res) => {
   try {
     const page =
@@ -118,59 +53,6 @@ router.get("/:address", async (req, res) => {
     console.error(err);
     res.sendStatus(500);
   }
-});
-
-router.get("/:address/feed", async (req, res) => {
-  const { address } = req.params;
-
-  if (!isAddress(address)) return res.sendStatus(400);
-
-  const feed = await PriceFeed.findOne({ address: address });
-
-  if (!feed) {
-    const token = await Token.findOne({ address: address });
-    if (!token) return res.sendStatus(404);
-
-    const config = await Config.findOne({});
-
-    if (!config) return res.sendStatus(500);
-
-    const newFeed = await PriceFeed.create({
-      address: address,
-      data: [],
-      lastRefreshedBlock: config.startBlock,
-    });
-    await newFeed.save();
-
-    return res.status(200).send({ data: newFeed.data });
-  }
-
-  const eventsFeed = await evm.client.getContractEvents({
-    ...contracts.token,
-    address: address,
-    eventName: "PriceChange",
-    fromBlock: feed.lastRefreshedBlock,
-    toBlock: "latest",
-  });
-
-  eventsFeed.forEach(
-    async (item) =>
-      await PriceFeed.updateOne(
-        { address: address },
-        {
-          $set: { lastRefreshedBlock: item.blockNumber + BigInt(1) },
-          $push: {
-            data: {
-              time: Number(item.args.time),
-              value: Number(item.args.value) / Number(ONE_FRAX),
-              mktCap: Number(item.args.marketCap) / Number(ONE_FRAX),
-            },
-          },
-        }
-      )
-  );
-
-  return res.status(200).send({ data: feed.data });
 });
 
 router.get("/by-user/:address", async (req, res) => {
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,5 +1,8 @@
+import { getContract } from "viem";
+import contracts from "../../contracts";
 import evm from "../../evm";
 import Config from "../models/Config";
+import Token from "../models/Token";
 
 export async function ensureConfig() {
   const configExists = await Config.countDocuments();
@@ -10,5 +13,68 @@ export async function ensureConfig() {
       startBlock: ((await evm.getBlockNumber()) * BigInt(8)) / BigInt(10),
     });
     await newConfig.save();
+  }
+}
+
+export async function refreshTokens() {
+  try {
+    const config = await Config.findOne({});
+    if (!config) throw "No config found";
+
+    const tokens = await evm.client.getContractEvents({
+      ...contracts.pumpItFaxtInterface,
+      eventName: "Launch",
+      fromBlock: config.tokensLastBlock,
+      toBlock: "latest",
+    });
+
+    tokens.forEach(async (item) => {
+      const tokenAddress = item.args.token;
+
+      if (!tokenAddress) return;
+
+      const token = getContract({
+        abi: contracts.token.abi,
+        address: tokenAddress,
+        client: evm.client,
+      });
+
+      const metadata = JSON.parse(await token.read.metadata());
+
+      await Token.deleteOne({ address: token.address });
+
+      const newToken = await Token.create({
+        address: token.address,
+        creator: await token.read.creator(),
+        name: await token.read.name(),
+        totalSupply: Number((await token.read.totalSupply()) / evm.ONE_TOKEN),
+        symbol: await token.read.symbol(),
+        image: await token.read.image(),
+        description: metadata.description,
+        telegram: metadata.telegram,
+        twitter: metadata.twitter,
+        website: metadata.website,
+      });
+
+      await newToken.save();
+
+      const updatedConfig = await Config.findOneAndUpdate(
+        {},
+        { $set: { tokensLastBlock: item.blockNumber } }
+      );
+      updatedConfig?.save();
+
+      // await PriceFeed.deleteOne({ address: token.address });
+
+      // const priceFeed = await PriceFeed.create({
+      //   address: token.address,
+      //   lastRefreshedBlock: await evm.getBlockNumber(),
+      //   data: [{ time: Date.now() - 1000 * 60, mktCap: 0, value: 0 }],
+      // });
+
+      // await priceFeed.save();
+    });
+  } catch (err) {
+    console.error(err);
   }
 }
